ebf09c5d84225fc49eed76cb337f9add1c8f7f7d
bijeshroy77 <bijesh.roy@gmail.com>
2017-07-18 02:16:53 +0530

    Product Catalog Commit

---
+++ src/main/java/com/shoppingcart/apps/repository/ProductRepository.java
@@ -0,0 +1,20 @@
+package com.shoppingcart.apps.repository;
+
+//import org.springframework.data.repository.CrudRepository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.shoppingcart.apps.entity.Product;
+
+//import com.shoppingcart.apps.entity.Account;
+
+
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+@Repository
+public interface ProductRepository extends CrudRepository<Product, String> {
+
+}
